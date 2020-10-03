4fae6584e8d45727dc9661c761627b67aa0bbed8
unknown <raqel.rhr@gmail.com>
2020-10-02 19:54:04 -0500

    primer commit

---
+++ prueba.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Mi página de prueba</title>
+  </head>
+  <body>
+    <h1>BIENVENIDO</h1>
+    <p>Esta es una página de prueba</p>
+    <?php echo '<p>Hola Mundo</p>'; ?>
+  </body>
+</html>
